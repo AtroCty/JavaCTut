36ad6efbc5242ca521ae51234255488a89bae63b
ATROCTY-CV\AtroCty-CV <atrocty@gmx.de>
2017-11-05 15:10:09 +0100

    Die richtige Korrektur (Max)

--- Java/Binomial.java
+++ Java/Binomial.java
@@ -1,35 +1,42 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package binomial;
-
+	
 /**
  *
  * @author max
+ * Korregiert von Timm
  */
-public class Binomial {
-        
-    public static int fakultaet(int zahl1)
-    {   
-    int fakul=1;
-        for (int i=1; i<=zahl1; i++){
-             fakul = i*i;
-        }
-        return fakul;
-    }
-    
-    public static int binomial(int n, int k){
-        return fakultaet(n)/(fakultaet(k)*fakultaet(n-k));
-    }
+public class Binomial
+{
+	public static float fakultaet(float zahl1)
+	{
+		float fakul=1;
+		for (float i=1; i<=zahl1; i++)
+		{
+			fakul = i*i;
+		}
+		// DEBUG
+		System.out.print("Das fakul ist "+fakul+"\n");
+		return fakul;
+	}
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        int ergebnis; int n=9; int k=2;
-            ergebnis=binomial(n, k);
-            System.out.print("Das Ergebnis ist"+" "+ergebnis+"\n");
-        }
-    }
+	public static float binomial(float n, float k)
+	{
+		// DEBUG
+		System.out.print("Das n ist "+n+"\n");
+		// DEBUG
+		System.out.print("Das k ist "+k+"\n");
+		// DEBUG
+		System.out.print("Das fakultaet(n) ist "+fakultaet(n)+"\n");
+		return (fakultaet(n)/(fakultaet(k)*fakultaet(n-k)));
+	}
+
+	/**
+	 * @param args the command line arguments
+	 */
+	public static void main(String[] args)
+	{
+		float ergebnis; float n=9; float k=2;
+		ergebnis=binomial(n, k);
+		System.out.print("Das Ergebnis ist "+ergebnis+"\n");
+	}
+}
